e6b649af0a69d0ccae889e5d556b4f450f1f3200
Antonio Perez Barrero <antonio.perez.barrero@ericsson.com>
2013-08-01 15:56:13 +0200

    Unit tests for DereferenceDelete functor.

--- tests/cpputiltests.cc
+++ tests/cpputiltests.cc
@@ -1,7 +1,25 @@
 #include <cpputil.hh>
 #include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
 
-TEST(Dummy, Test)
+using namespace cpputil;
+
+TEST(DereferenceDeleteTests, DeleteSinglePointer_Works)
 {
+    int *pointer = new int;
+
+    DereferenceDelete<int> deleter;
+    deleter(pointer);
     SUCCEED();
 }
+
+TEST(DereferenceDeleteTests, DeleteContainer_Works)
+{
+    std::vector<int*> v;
+    for (unsigned int i = 0; i < 10; ++i) v.push_back(new int);
+
+    std::for_each(v.begin(), v.end(), DereferenceDelete<int>());
+    SUCCEED();
+}
+
